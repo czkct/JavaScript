628bdf4ba6461c01467f633185e32d0eab47bc80
dyh <2504621508@qq.com>
2016-08-01 17:58:49 +0800

    用正则表达式验证电子邮件地址

---
+++ test16_8_1_09_58/test.js
@@ -0,0 +1,69 @@
+window.onload = function() {
+	document.forms[0].onsubmit = validForm;
+}
+
+function validForm() {
+	var allGood  =true;
+	var allTags = document.forms[0].getElementsByTagName("*");
+
+	for(var i = 0; i < allTags.length; i++) {
+		if(!validTag(allTags[i])) {
+			allGood = false;
+		}
+	}
+	return allGood;  //返回true或false  让onsubmit知道是否要提交
+
+	function validTag(thisTag) {
+		var outClass = "";
+		var allClasses = thisTag.className.split(" ");  //将一个标签class中的所有类放进allClasses集合中
+
+		for(var j = 0; j < allClasses.length; j++) {
+			outClass += validBasedOnClass(allClasses[j]) + " ";
+		}
+
+		thisTag.className = outClass;
+
+		if(outClass.indexOf("invalid") > -1) {
+			invalidLabel(thisTag.parentNode);
+			thisTag.focus();
+			if(thisTag.nodeName == "INPUT") {
+				thisTag.select();
+			}
+			return false;
+		}
+		return true;
+
+		function validBasedOnClass(thisClass) {
+			var classBack = "";
+
+			switch(thisClass) {
+				case "":
+				case "invalid":
+					break;
+				case "email":
+					if(allGood && !validEmail(thisTag.value)) {
+						classBack = "invalid ";
+					}
+					classBack += thisClass;
+					break;
+				default:
+					classBack += thisClass;
+			}
+			return classBack;
+		}
+
+		function validEmail(email) {
+			var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+			return re.test(email);
+		}
+
+		function invalidLabel(parentTag) {
+			if(parentTag.nodeName == "LABEL") {
+				parentTag.className += " invalid";
+			}
+		}
+	}
+}
+
+//多使用alert进行调试！！！
