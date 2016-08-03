2bce48bd175516d796b9955985e2e10151ac18aa
dyh <2504621508@qq.com>
2016-08-03 15:49:33 +0800

    onfocus鼠标事件 ----举例：使用onfocus事件迫使用户离开这个字段， 从而避免用户修改他们不应该修改的字段

--- test16_08_03_10_54/test.js
+++ test16_08_03_10_54/test.js
@@ -4,7 +4,7 @@ function initForm() {
 	var allTags = document.forms[0].getElementsByTagName("*");
 
 	for(var i = 0; i < allTags.length; i++) {
-		if(allTags[i].readonly) {
+		if(allTags[i].readOnly) {
 			allTags[i].onfocus = function() {
 				this.blur();
 			}
